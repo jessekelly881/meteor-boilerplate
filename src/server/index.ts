5c2427216311cbd646db3e7bd62e3a7f92ad786e
Jesse Kelly <kellyjesse881@gmail.com>
2020-09-30 13:40:37 -0500

    chore: changed folder structure

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,5 @@
 import { Meteor } from "meteor/meteor";
-import { LinksCollection } from "/lib/api/links";
+import { LinksCollection } from "/src/models/links";
 
 function insertLink({ title, url }) {
     LinksCollection.insert({ title, url, createdAt: new Date() });
@@ -11,21 +11,6 @@ Meteor.startup(() => {
         insertLink({
             title: "Do the Tutorial",
             url: "https://www.meteor.com/tutorials/react/creating-an-app",
-        });
-
-        insertLink({
-            title: "Follow the Guide",
-            url: "http://guide.meteor.com",
-        });
-
-        insertLink({
-            title: "Read the Docs",
-            url: "https://docs.meteor.com",
-        });
-
-        insertLink({
-            title: "Discussions",
-            url: "https://forums.meteor.com",
         });
     }
 });
