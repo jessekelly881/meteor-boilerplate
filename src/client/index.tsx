b691987cc24dac0ddb25ffe259c51bfcf1d924fb
Jesse Kelly <kellyjesse881@gmail.com>
2020-10-01 17:53:33 -0500

    feat: added server auth code

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,10 +1,12 @@
 import React, { useState } from "react";
 import { Meteor } from "meteor/meteor";
+import { useTracker } from "meteor/react-meteor-data";
 import { render } from "react-dom";
 
 const LoginForm = () => {
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+    const user = useTracker(() => Meteor.user());
 
     const submit = e => {
         e.preventDefault();
@@ -13,38 +15,36 @@ const LoginForm = () => {
     };
 
     return (
-        <form onSubmit={submit} className="login-form">
-            <label htmlFor="username">Username</label>
+        <>
+            <form onSubmit={submit} className="login-form">
+                <label htmlFor="username">Username</label>
 
-            <input
-                type="text"
-                placeholder="Username"
-                name="username"
-                required
-                onChange={e => setUsername(e.target.value)}
-            />
+                <input
+                    type="text"
+                    placeholder="Username"
+                    name="username"
+                    required
+                    onChange={e => setUsername(e.target.value)}
+                />
 
-            <label htmlFor="password">Password</label>
+                <label htmlFor="password">Password</label>
 
-            <input
-                type="password"
-                placeholder="Password"
-                name="password"
-                required
-                onChange={e => setPassword(e.target.value)}
-            />
+                <input
+                    type="password"
+                    placeholder="Password"
+                    name="password"
+                    required
+                    onChange={e => setPassword(e.target.value)}
+                />
 
-            <button type="submit">Log In</button>
-        </form>
+                <button type="submit">Log In</button>
+            </form>
+            <button onClick={Meteor.logout}>Logout</button>
+            <span>{user?.username || "Anon"}</span>
+        </>
     );
 };
 
 Meteor.startup(() => {
-    render(
-        <>
-            <LoginForm />
-            <span>{Meteor.userId() || "Anon"}</span>
-        </>,
-        document.getElementById("app"),
-    );
+    render(<LoginForm />, document.getElementById("app"));
 });
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,16 +1,14 @@
 import { Meteor } from "meteor/meteor";
-import { LinksCollection } from "/src/models/links";
+import { Accounts } from "meteor/accounts-base";
 
-function insertLink({ title, url }) {
-    LinksCollection.insert({ title, url, createdAt: new Date() });
-}
+const SEED_USERNAME = "meteorite";
+const SEED_PASSWORD = "password";
 
 Meteor.startup(() => {
-    // If the Links collection is empty, add some data.
-    if (LinksCollection.find().count() === 0) {
-        insertLink({
-            title: "Do the Tutorial",
-            url: "https://www.meteor.com/tutorials/react/creating-an-app",
+    if (!Accounts.findUserByUsername(SEED_USERNAME)) {
+        Accounts.createUser({
+            username: SEED_USERNAME,
+            password: SEED_PASSWORD,
         });
     }
 });
