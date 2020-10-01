54b723a281fff924f70901d65e0fd89359f5946b
Jesse Kelly <kellyjesse881@gmail.com>
2020-10-01 17:44:53 -0500

    feat: added client login form

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,10 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { Meteor } from "meteor/meteor";
 import { render } from "react-dom";
 
+const LoginForm = () => {
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+
+    const submit = e => {
+        e.preventDefault();
+
+        Meteor.loginWithPassword(username, password);
+    };
+
+    return (
+        <form onSubmit={submit} className="login-form">
+            <label htmlFor="username">Username</label>
+
+            <input
+                type="text"
+                placeholder="Username"
+                name="username"
+                required
+                onChange={e => setUsername(e.target.value)}
+            />
+
+            <label htmlFor="password">Password</label>
+
+            <input
+                type="password"
+                placeholder="Password"
+                name="password"
+                required
+                onChange={e => setPassword(e.target.value)}
+            />
+
+            <button type="submit">Log In</button>
+        </form>
+    );
+};
+
 Meteor.startup(() => {
     render(
-        <h1>Simple Meteor App with React</h1>,
+        <>
+            <LoginForm />
+            <span>{Meteor.userId() || "Anon"}</span>
+        </>,
         document.getElementById("app"),
     );
 });
