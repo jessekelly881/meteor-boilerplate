25d59f7f78b1e16ae4340669cd2be9e3074c75fc
Jesse Kelly <kellyjesse881@gmail.com>
2020-10-19 13:19:40 -0500

    chore(server): removed default user creating

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,19 +1,6 @@
-import { Meteor } from "meteor/meteor";
 import { Accounts } from "meteor/accounts-base";
 
 Accounts.config({
+    // @see https://docs.meteor.com/api/accounts-multi.html#AccountsCommon-config
     sendVerificationEmail: true,
-});
-
-const SEED_EMAIL = "meteorite@m.com";
-const SEED_PASSWORD = "password";
-
-Meteor.startup(() => {
-    if (!Accounts.findUserByUsername(SEED_EMAIL)) {
-        Accounts.createUser({
-            username: SEED_EMAIL,
-            email: SEED_EMAIL,
-            password: SEED_PASSWORD,
-        });
-    }
 });
