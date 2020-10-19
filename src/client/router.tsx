9487bdc922bf57a02653be370c77287e6522e069
Jesse Kelly <kellyjesse881@gmail.com>
2020-10-19 12:51:31 -0500

    feat: added matchAppRoute

--- src/client/router.tsx
+++ src/client/router.tsx
@@ -1,6 +1,10 @@
+import { matchConfig } from "@babakness/exhaustive-type-checking";
+
 export enum AppRoute {
     login = "/auth/login",
     signup = "/auth/signup",
     forgotPass = "/auth/forgotPass",
     validateEmail = "/auth/validateEmail",
 }
+
+export const matchAppRoute = matchConfig<AppRoute>();
