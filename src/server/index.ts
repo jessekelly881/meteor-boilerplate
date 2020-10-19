127ce39abebcc48caafcfc0d0c71fecf9a0b2f81
Jesse Kelly <kellyjesse881@gmail.com>
2020-10-19 13:38:18 -0500

    feat: created /src/config.ts

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,9 @@
 import { Accounts } from "meteor/accounts-base";
+import config from "/src/config";
 
 Accounts.config({
     // @see https://docs.meteor.com/api/accounts-multi.html#AccountsCommon-config
     sendVerificationEmail: true,
 });
+
+Accounts.emailTemplates.siteName = config.siteName();
