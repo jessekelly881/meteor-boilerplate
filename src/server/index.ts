c4a8a1793f0d5f6c7ae57c249a5c8181921335f1
Jesse Kelly <kellyjesse881@gmail.com>
2020-10-19 22:38:30 -0500

    feat: added passwordResetTokenExpirationInDays:

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,9 +1,10 @@
 import { Accounts } from "meteor/accounts-base";
 import config from "/src/config";
 
+// @see https://docs.meteor.com/api/accounts-multi.html#AccountsCommon-config
 Accounts.config({
-    // @see https://docs.meteor.com/api/accounts-multi.html#AccountsCommon-config
     sendVerificationEmail: true,
+    passwordResetTokenExpirationInDays: 2,
 });
 
 Accounts.emailTemplates.siteName = config.app.name;
